44f6904ce183cb261fa1f5862d6b0f2f314d558d
Elizabeth Engelman <elizabeth@redsquirrel.com>
2022-09-20 22:27:35 -0400

    Add US d3 map as a starting point for a map of Lanc

---
+++ week-2/map.js
@@ -0,0 +1,49 @@
+// https://www.youtube.com/watch?v=hrJ64jpYb0A
+//https://flaviocopes.com/fix-cannot-use-import-outside-module/
+// https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules/
+const width = 975;
+const height = 610;
+import data from "./data.json" assert { type: "json" };
+import us from "./us.json" assert { type: "json" };
+
+const path = d3.geoPath();
+const svg = d3.create("svg").attr("width", width).attr("height", height);
+const projection = d3
+  .geoAlbersUsa()
+  .scale(1300)
+  .translate([width / 2, height / 2]);
+
+const statesBackground = svg
+  .append("path")
+  .attr("fill", "#ddd")
+  .attr("d", path(topojson.feature(us, us.objects.nation)));
+
+const statesBorders = svg
+  .append("path")
+  .attr("fill", "none")
+  .attr("stroke", "#fff")
+  .attr("stroke-linejoin", "round")
+  .attr("stroke-linecap", "round")
+  .attr("d", path(topojson.mesh(us, us.objects.states, (a, b) => a !== b)));
+
+const stateCapitalElements = svg.selectAll("g").data(data).join("g");
+
+const capitalGroups = stateCapitalElements
+  .append("g")
+  .attr(
+    "transform",
+    ({ longitude, latitude }) =>
+      `translate(${projection([longitude, latitude]).join(",")})`
+  );
+
+capitalGroups.append("circle").attr("r", 2);
+
+capitalGroups
+  .append("text")
+  .attr("text-anchor", "middle")
+  .attr("font-family", "sans-serif")
+  .attr("font-size", 10)
+  .attr("y", -6)
+  .text(({ description }) => description);
+
+document.body.appendChild(svg.node());
