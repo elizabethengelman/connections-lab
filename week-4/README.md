dfe4ab62550b7d05cd6b7588bbc7c1dd274d183b
Elizabeth Engelman <elizabeth@redsquirrel.com>
2022-09-29 08:00:55 -0400

    Very mess wip, but I'm getting closer

---
+++ week-4/README.md
@@ -0,0 +1,26 @@
+# Week 4
+
+## References:
+
+This week I'll aim to get the trees mapped, and allow a user to click on a tree to see the info.
+
+
+https://medium.datadriveninvestor.com/getting-started-with-d3-js-maps-e721ba6d8560
+
+https://www.tutorialsteacher.com/d3js/create-svg-elements-in-d3js
+
+https://www.d3indepth.com/geographic/
+
+
+has a bounding box:
+
+https://nominatim.openstreetmap.org/search.php?city=Lancaster&state=Pennsylvania&format=jsonv2
+
+
+i think that part of the reason that my map is so weird, is because lancaster2.geojson includes data from a bunch of streets with lancaster somewhere in the record! duh!
+
+https://mapservices.pasda.psu.edu/server/rest/services/pasda/LancasterCounty/MapServer
+
+https://www.pasda.psu.edu/uci/DataSummary.aspx?dataset=1267
+
+https://www.pasda.psu.edu/
---
+++ week-4/lanc-map.js
@@ -0,0 +1,34 @@
+const margin = 20;
+const width = 975;
+const height = 610;
+
+d3.json("./LancasterCountyRDCLINE202209.geojson").then(function (data) {
+  const streets = data;
+  const cityProjection = d3.geoMercator().fitExtent(
+    [
+      [margin, margin],
+      [width - margin, height - margin],
+    ],
+    streets
+  );
+  const cityPathGenerator = d3.geoPath().projection(cityProjection);
+
+  const svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  // construct the path elements using the D3 data join
+  svg
+    .selectAll("path")
+    // data() expects an Array, so make sure to pass the features entry of our FeatureCollection
+    .data(streets.features)
+    // select all data items that are not represented on the map yet, and add them
+    .enter()
+    .append("path")
+    .attr("d", cityPathGenerator)
+    .attr("fill", "none")
+    .attr("stroke", "#999999")
+    .attr("stroke-width", "0.5");
+});
